32ef6cc263bdb8d890b728166f0f3601484782e7
Leon Welchert <lichturm@posteo.de>
2025-02-03 01:19:13 +0100

    add line numbers to nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -23,6 +23,11 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- vim.o is equivalent to :set
+vim.o.wrap = true
+vim.o.number = true
+vim.o.relativenumber = true
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
